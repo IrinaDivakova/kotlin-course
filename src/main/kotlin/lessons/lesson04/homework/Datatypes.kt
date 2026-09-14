4b2daf3feb3cf3dee11a43f22cd8c19b6fba9d98
Irina_D <bibber14@mail.ru>
2026-09-14 11:21:34 +0300

    1.file Datatypes.kt added

---
+++ src/main/kotlin/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,76 @@
+package org.example.lessons.lesson04.homework
+
+val v1: Int = 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+val v6: Boolean = false // no capital letters like FALSE
+val v7: Char = 'c'
+val v8: Int = 500
+val v9: Long  = 4294967296L
+val v10: Float = 18.0f
+val v11 = -0.001
+val v12: String = "OpenAI"
+val v13: String = "true"
+val v14 = listOf(3, 14)
+val v15: Char  = '9'
+val v16 = 2048
+val v17: Long  = 10000000000L
+val v18 = setOf("OpenAI", "Quantum Computing")
+val v19: Float = 5.75f
+//val v20 = `1.414`
+val v20: Double = 1.414 // no italics for Double
+val v21: String = "Artificial Intelligence"
+val v22: Array<Any> = arrayOf('x', "A")
+val v23: String = "Android Studio"
+val v24: Char  = '@'
+val v25: Int = 1024
+val v26: Long  = 1234567890123L
+val v27: Float = 10.01f
+val v28: Double = -273.15
+val v29: String = "SpaceX"
+val v30: Boolean = false // no capital letters like FALSE
+val v31: Double = 0.007
+//val v32: Char  = '🤯' // chars between '' not “🤯”
+val v32: String = "🤯"
+val v33: Map<String, Int> = mapOf("true" to 2, "false" to 34)
+//val v34 = ‘65535’
+val v34: Int = 65535// no italics for Int
+val v35: Long  = 72057594037927935L
+val v36: Float = 2.71828f
+val v37: Double = 101.0101
+val v38: String = "Quantum Computing"
+val v39: Map<Int, String> = mapOf(2 to "true", 34 to "false")
+val v40: Char = 'x'
+val v41: Int = 314
+val v42: Long  = 123456789123456789L
+val v43: Float = 6.626f
+val v44: Boolean = true // no capital letters
+
+//Подбери подходящий тип который будет:
+//
+//Хранить букву, на которую указывает палец медиума во время спиритического сеанса.
+var a: Char = 'f'
+//Хранить количество ложек сахара, которые я кладу в одну чашку чая.
+var b: Short = 0
+//Хранить список расходов на доставку еды, чтобы ещё раз убедиться, что готовить было дешевле.
+var c: List<Double> = listOf(12.2, 14.59, 4.89)
+//Хранить длину очереди в столовой до миллиардной доли сантиметра.
+var d: Double = 3000.4545156
+//Хранить факт, закрыт ли баг после того, как его просто переименовали в фичу.
+var isFixed: Boolean = true
+//Хранить количество свистков чайника за день.
+var e: Int = 12
+//Хранить количество нажатий котом на клавиатуру ноутбука за всё время твоей работы.
+var f: Short = 0
+//Хранить количество попыток пересчитать звёзды на небе за всю историю человечества.
+var g: Long = 145889999999L
+//Хранить массу воздуха в спускающем матрасе после нападения кота в долях грамма.
+var h: Float = 1452.458f
+//Хранить словарь «название стартапа → сумму потерь инвесторов».
+var i: Map<String, Double> = mapOf("Bite" to 14589.45)
+//Хранить строку «починилось само», чтобы закрывать тикеты без лишних слов.
+var j: String = "починилось само"
+//Хранить список тем для митингов, которые на самом деле никому не нужны.
+var k: List<String> = listOf("hjfkdhkj", "bfdjghj")
